9bc609d1ac2eb3a6426292f8c08d5a25ba2a24ac
Folka2134 <folka2134@gmail.com>
2023-11-21 17:06:36 +0000

    creating header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import Logo from "../assets/Logo.png";
+
 export const Header = () => {
   return (
-    <div>
-      <p>header</p>
+    <div className="flex h-16 items-center justify-between bg-[#471ca8] py-8">
+      <img src={Logo} width={64} height={64} alt="logo" />
+      <div className="flex gap-4 pr-24">
+        <button>sign-in</button>
+        <button>sign-out</button>
+      </div>
     </div>
   );
 };
