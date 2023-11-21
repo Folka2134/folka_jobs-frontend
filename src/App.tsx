2304912ff33d3d637fef788908b76e1bf9d44b6f
Folka2134 <folka2134@gmail.com>
2023-11-21 17:06:54 +0000

    displaying footer and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen">
       <Header />
-    </>
+      <main></main>
+      <Footer />
+    </div>
   );
 }
 
